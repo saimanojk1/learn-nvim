fc844ef3c06dc241f075ac56e72d820ce5b72612
saimanojk1 <saimanojk1@hotmail.com>
2024-04-08 15:14:27 +0200

    Updated config with git stuff

--- nvim-config/lua/plugins/neo-tree.lua
+++ nvim-config/lua/plugins/neo-tree.lua
@@ -1,23 +1,35 @@
-return   {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      event_handlers = {
-        {
-          -- auto close
-          event = "file_opened",
-          handler = function()
-            vim.cmd("Neotree close")
-          end
-        },
-      }
-    })
-    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-  end
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					-- auto close
+					event = "file_opened",
+					handler = function()
+						vim.cmd("Neotree close")
+					end,
+				},
+			},
+			filtered_items = {
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				hide_by_name = {
+					"node_modules",
+				},
+				never_show = {
+					".DS_Store",
+					"thumbs.db",
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+	end,
 }
