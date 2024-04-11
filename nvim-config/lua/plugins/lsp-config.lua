291af5d25329785ca586079ee916cc5d2d354d49
saimanojk1 <34206385+saimanojk1@users.noreply.github.com>
2024-04-11 08:15:51 +0200

    Add auto setup for lsp servers

--- nvim-config/lua/plugins/lsp-config.lua
+++ nvim-config/lua/plugins/lsp-config.lua
@@ -11,6 +11,11 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "tsserver" },
       })
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          require("lspconfig")[server_name].setup {}
+        end
+      })
     end,
     opts = {
       auto_install = true,
